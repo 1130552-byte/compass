00878eda8c5fee60efd1cb64552b714296705507
1130552-byte <1130552@pdsb.net>
2026-05-13 09:04:43 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,14 @@
+degree = 0
+
+def on_forever():
+    global degree
+    degree = input.compass_heading()
+    if degree < 45 or degree > 315:
+        basic.show_string("N")
+    elif degree < 135:
+        basic.show_string("E")
+    elif degree < 225:
+        basic.show_string("s")
+    else:
+        basic.show_string("W")
+basic.forever(on_forever)
